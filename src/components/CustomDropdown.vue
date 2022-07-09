0833d23ae2973770f5e4b98b77548dd8fe47e5c4
chanksela <kakhachankseliani@gmail.com>
2022-07-09 11:55:06 +0400

    created simple component CustomDropdown. displaing characters with names and images. now need to add css and maybe a little javascript logic, so they are displaid only when i click on 'choose ur character'

--- src/components/CustomDropdown.vue
+++ src/components/CustomDropdown.vue
@@ -1,7 +1,48 @@
-<template>This is a custom dropdown</template>
+<template>
+  <div class="dropdown">
+    <p>Choose your character</p>
+    <div class="dropdown-select">
+      <div
+        class="dropdown-option"
+        v-for="character in characters"
+        v-bind:key="character.id"
+      >
+        <p>{{ character.name }}</p>
+        <img
+          :src="'https://chess-tournament-api.devtest.ge' + character.image"
+        />
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
-export default { name: "CustomDropdown" };
+export default {
+  name: "CustomDropdown",
+  data() {
+    return {
+      characters: [],
+    };
+  },
+  mounted() {
+    fetch("https://chess-tournament-api.devtest.ge/api/grandmasters")
+      .then((res) => res.json())
+      .then((data) => ((this.characters = data), console.log(data)))
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+img {
+  height: 50px;
+  width: auto;
+}
+.dropdown-option {
+  border: red solid 1px;
+  display: flex;
+  flex-direction: row;
+
+  align-items: center;
+}
+</style>
